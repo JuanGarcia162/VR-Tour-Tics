6e7d9fa2a2d10bf604a8409ce0dec8c048b60cbb
JuanGarcia162 <juadgarca@misena.edu.co>
2022-05-09 21:37:30 -0500

    Proyecto Base

---
+++ Assets/EXPOVR/Scripts/Artwork.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Artwork : MonoBehaviour
+{
+	[Header ("Artworks")]
+	public ArtworkSO artworkSO;
+	private int materialId = 1;
+	MeshRenderer[] artworks;
+
+	private void Start ()
+	{
+		artworks = GetComponentsInChildren<MeshRenderer> ();
+		SetArtworks ();
+	}
+
+	private void SetArtworks ()
+	{
+		for (int i = 0; i < artworks.Length; i++)
+		{
+			artworks[i].materials[materialId].mainTexture = artworkSO.artworks[i];
+		}
+	}
+
+}
